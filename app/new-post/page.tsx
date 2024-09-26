2aee9ac1b8618d22fdb3af27372d53becaf96acb
AhmedAhmed00 <devahmedahmed@gmail.com>
2024-09-26 17:46:43 +0300

    init project

---
+++ app/new-post/page.tsx
@@ -0,0 +1,39 @@
+// storePost({
+//   imageUrl: "",
+//   title,
+//   content,
+//   userId: 1,
+// });
+
+export default function NewPostPage() {
+  // server action to create new post
+
+  return (
+    <>
+      <h1>Create a new post</h1>
+      <form>
+        <p className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" />
+        </p>
+        <p className="form-control">
+          <label htmlFor="image">Image URL</label>
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            id="image"
+            name="image"
+          />
+        </p>
+        <p className="form-control">
+          <label htmlFor="content">Content</label>
+          <textarea id="content" name="content" />
+        </p>
+        <p className="form-actions">
+          <button type="reset">Reset</button>
+          <button>Create Post</button>
+        </p>
+      </form>
+    </>
+  );
+}
