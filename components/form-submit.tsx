ff56306632213ff4b05809fa2711326893ea7d2e
AhmedAhmed00 <devahmedahmed@gmail.com>
2024-10-27 17:41:14 +0300

    finish

--- components/form-submit.tsx
+++ components/form-submit.tsx
@@ -4,13 +4,8 @@ import { useFormStatus } from 'react-dom';
 
 export default function FormSubmit(){ 
   const status = useFormStatus()
-  
-  if(status.pending){ 
-    return <div>create</div>
-  }
-    
     return <>
     <button type="reset">Reset</button>
-    <button disabled={status.pending}>{status.pending ? "loaad" : "Create Post"}</button>
+    <button disabled={status.pending}>{status.pending ? "Loaaaing ..." : "Create Post"}</button>
     </>
 }
